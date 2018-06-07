45677f763d2658409911a99b8ab07aeccb8d4889
Wanderson Duarte <wandersonduarte.br@gmail.com>
2018-06-06 22:55:24 -0300

    CourseManage

--- simplemooc/courses/models.py
+++ simplemooc/courses/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 
+class CourseManager(models.Manager):
+    def search(self, query):
+        return self.get_queryset().filter(
+            models.Q(name__icontains=query) | \
+            models.Q(description__icontains=query)
+        )
+
 class Course(models.Model):
     name = models.CharField('Nome', max_length=100)
     slug = models.SlugField('Atalho')
@@ -13,6 +20,11 @@ class Course(models.Model):
 
     created_at = models.DateTimeField('Criado em', auto_now_add=True)
     updated_at = models.DateTimeField('Atualizado em', auto_now_add=True)
+
+    objects = CourseManager()
+
+    def __str__(self):
+        return self.name
 
 
 
