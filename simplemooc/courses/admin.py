b461c57042a8913be1ee8a25b21742eb3a216ac2
Wanderson Duarte <wandersonduarte.br@gmail.com>
2018-06-07 22:27:40 -0300

    Criado CoursesModelAdmin

--- simplemooc/courses/admin.py
+++ simplemooc/courses/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Course
 
-admin.site.register(Course)
+class CoursesAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'created_at', 'updated_at']
+    list_search = ['name', 'slug']
+    prepopulated_fields = {'slug':('name',)}
+
+admin.site.register(Course, CoursesAdmin)
 
--- simplemooc/courses/models.py
+++ simplemooc/courses/models.py
@@ -26,5 +26,9 @@ class Course(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = 'curso'
+        verbose_name_plural = 'cursos'
+
 
 
